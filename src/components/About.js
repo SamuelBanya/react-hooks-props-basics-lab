a78cf9f338185c4d15e272c13e744498aeea8aa0
Samuel Banya <sbanya@fastmail.com>
2022-06-24 20:40:33 -0400

    Adding all changes including custom function to assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,25 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  function determineIfBioIsBlank(props) {
+    console.log("props: ");
+    console.log(props);
+    if (!props.bio) {
+      return "";
+    } else {
+      return (
+        <p>{props.bio}</p>
+    );
+    }
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {determineIfBioIsBlank(props)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
